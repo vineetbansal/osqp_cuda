ea4c9226c2e9575790109a01d417832535e7c27d
Vineet Bansal <vineetbansal@protonmail.com>
2022-01-25 13:34:06 -0500

    vs integration for cuda

--- .github/workflows/prepare_build_environment_windows_cuda.sh
+++ .github/workflows/prepare_build_environment_windows_cuda.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_461.33_win10.exe
-./cuda.exe -s nvcc_11.2 cudart_11.2 cublas_dev_11.2 curand_dev_11.2
+./cuda.exe -s nvcc_11.2 cudart_11.2 cublas_dev_11.2 curand_dev_11.2 visual_studio_integration_11.2
 rm cuda.exe
